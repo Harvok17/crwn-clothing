38da89f4b5ef0a37f79b7a0e1a2d3d5056901362
Harvey Granfil <jhgranfil@gmail.com>
2021-05-31 18:09:37 +0800

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,6 +4,8 @@ import React from "react";
 
 import CollectionItem from "../collection-item/collection-item.component";
 
+// Add scrolling feature here
+
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
