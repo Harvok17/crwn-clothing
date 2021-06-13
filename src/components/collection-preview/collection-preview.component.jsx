e354956a03ddcbb25660d9a8eed48af967f8f06f
Harvey Granfil <jhgranfil@gmail.com>
2021-06-14 04:16:30 +0800

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,13 +3,16 @@ import "./collection-preview.styles.scss";
 import React from "react";
 
 import CollectionItem from "../collection-item/collection-item.component";
+import { OptionLink } from "../header/header.styles";
 
 // Add scrolling feature here
 
-const CollectionPreview = ({ title, items }) => {
+const CollectionPreview = ({ title, items, routeName }) => {
   return (
     <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
+      <OptionLink to={`/shop/${routeName}`} className="title">
+        {title.toUpperCase()}
+      </OptionLink>
       <div className="preview">
         {items
           .filter((_, idx) => idx < 4)
